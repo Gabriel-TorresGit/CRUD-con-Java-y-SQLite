65ac09d248c438d109f94bdfb3726bf64c89039e
gabriel <gabitorres64@gmail.com>
2022-07-04 11:26:27 -0300

    mostrar datos en la tabla de la interfaz

--- src/sistema/empleadosGUI/frmEmpleados.java
+++ src/sistema/empleadosGUI/frmEmpleados.java
@@ -5,6 +5,7 @@
  */
 package sistema.empleadosGUI;
 import java.sql.ResultSet;
+import javax.swing.table.DefaultTableModel;
 import sistema.empleadosBL.empleadosBL;
 import sistema.empleadosDAL.conexion;
 
@@ -13,12 +14,21 @@ import sistema.empleadosDAL.conexion;
  * @author Gabi
  */
 public class frmEmpleados extends javax.swing.JFrame {
+    
+    DefaultTableModel modelo;
 
     /**
      * Creates new form frmEmpleados
      */
     public frmEmpleados() {
         initComponents();
+        
+        String[] titulos = {"ID","Nombre","Correo"};
+        
+        modelo = new DefaultTableModel(null,titulos);
+        tblEmpleados.setModel(modelo);
+        
+        mostrarDatos();
     }
 
     /**
@@ -183,7 +193,29 @@ public class frmEmpleados extends javax.swing.JFrame {
             System.out.println(e);
         }  
     }//GEN-LAST:event_btnAgregarActionPerformed
-
+    
+    public void mostrarDatos(){
+        conexion objConexion = new conexion();
+        
+        try {
+            ResultSet resultado = objConexion.consultarRegistros("SELECT * FROM Empleados");
+            
+            while (resultado.next()) {
+                
+                System.out.println(resultado.getString("ID"));
+                System.out.println(resultado.getString("Nombre"));
+                System.out.println(resultado.getString("Correo"));
+                
+                Object[] oUsuario = {resultado.getString("ID"), resultado.getString("Nombre"), resultado.getString("Correo") };
+                
+                modelo.addRow(oUsuario);
+                
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    
     public empleadosBL recuperarDatosGUI(){
         empleadosBL oEmpleados = new empleadosBL();
         
